00b01c5c624931e4c26bd38c86ff87e90a91d4eb
Jaider Nicolás <jaider.perez@usantoto.edu.co>
2022-04-02 15:45:05 -0500

    fix: bug fixes

--- src/domain/constants/constants.ts
+++ src/domain/constants/constants.ts
@@ -12,7 +12,7 @@ export class Constants {
 
     public static TEACHER_DELETED_SUCCESSFULLY: string = 'teacher deleted successfully';
 
-    public static TEMPLATE_NOT_FOUND: string = 'template deleted successfully';
+    public static TEMPLATE_NOT_FOUND: string = 'template not found';
 
     public static TEMPLATE_UPDATE_SUCCESSFULLY: string = 'template update successfully';
 
