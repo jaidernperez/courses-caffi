62de1485f24c24706182e5e2b834b6eb4d3081de
Jaider Nicolás <jaider.perez@usantoto.edu.co>
2022-04-02 14:56:00 -0500

    feat: update application config

--- src/application/config/config.ts
+++ src/application/config/config.ts
@@ -1,25 +1,41 @@
 import 'reflect-metadata';
 import {Container} from 'inversify';
 
-import {Server, SlideController, TeacherController} from '../../infrastructure/entry-points/api-rest';
-import {SlideUseCase, TeacherUseCase} from '../../domain/usecases';
+import {
+    Server,
+    SlideController,
+    TeacherController,
+    TemplateController
+} from '../../infrastructure/entry-points/api-rest';
+import {SlideUseCase, TeacherUseCase, TemplateUseCase} from '../../domain/usecases';
 import {
     SlideAdapter,
     SlideDataMapper,
     SlideDataMapperImpl,
     TeacherAdapter,
     TeacherDataMapper,
-    TeacherDataMapperImpl
+    TeacherDataMapperImpl,
+    TemplateAdapter,
+    TemplateDataMapper,
+    TemplateDataMapperImpl
 } from '../../infrastructure/driven-adapters';
-import {SlideRepository, TeacherRepository} from '../../domain/models';
+import {SlideRepository, TeacherRepository, TemplateRepository} from '../../domain/models';
 import {
     SlideDomainMapper,
     SlideDomainMapperImpl,
     TeacherDomainMapper,
-    TeacherDomainMapperImpl
+    TeacherDomainMapperImpl,
+    TemplateDomainMapper,
+    TemplateDomainMapperImpl
 } from '../../domain/mappers';
 
 const container = new Container();
+
+container.bind<TemplateDomainMapper>(Symbol.for('TemplateDomainMapper')).to(TemplateDomainMapperImpl).inSingletonScope();
+container.bind<TemplateDataMapper>(Symbol.for('TemplateDataMapper')).to(TemplateDataMapperImpl).inSingletonScope();
+container.bind<TemplateRepository>(Symbol.for('TemplateRepository')).to(TemplateAdapter).inSingletonScope();
+container.bind<TemplateUseCase>(Symbol.for('TemplateUseCase')).to(TemplateUseCase).inSingletonScope();
+container.bind<TemplateController>(Symbol.for('TemplateController')).to(TemplateController).inSingletonScope();
 
 container.bind<TeacherDomainMapper>(Symbol.for('TeacherDomainMapper')).to(TeacherDomainMapperImpl).inSingletonScope();
 container.bind<TeacherDataMapper>(Symbol.for('TeacherDataMapper')).to(TeacherDataMapperImpl).inSingletonScope();
